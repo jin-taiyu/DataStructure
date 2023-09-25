98a1cf954125e0b89c1c5ab0f120e7334d0a35a1
Taiyu <222090218@stu.nau.edu.cn>
2023-09-25 14:51:51 +0800

    完成了选做函数

--- LinkList/linklist.h
+++ LinkList/linklist.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_1939BD45238D71C4
-#define HEADER_1939BD45238D71C4
+#ifndef HEADER
+#define HEADER
 
  typedef int ElemType;
 //单链表的存储定义
@@ -8,5 +8,6 @@ typedef struct LNode
     ElemType         data;
     struct LNode   *next;
 }LNode, *LinkList;
-#endif // header guard 
+
+#endif // header guard
 
